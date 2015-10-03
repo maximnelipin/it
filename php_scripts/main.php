f2b520d3a3bd52c07f197300e5e63c61edceff6a
maximnelipin <nelipin.maxim@yandex.ru>
2015-10-03 15:19:26 +0300

    Поправил коннект к базе

--- php_scripts/main.php
+++ php_scripts/main.php
@@ -1,7 +1,18 @@
 <?php
 	session_start();
 	if(isset($_SESSION['user_id']))
-	{phpinfo();
+	{try {
+		$conbd=new PDO('mysql:host=192.168.0.75;dbname=IT_INFO', 'itinfo', 'Passw0rd');
+		$conbd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$conbd->exec('SET NAMES "utf8"');
 	}
+	catch (PDOException $e)
+	{
+		echo 'Нет подключения к базе';
+		echo $e->getMessage();
+		exit;
+	}
+	echo 'Есть подключение к базе';	}
+	
 
 ?>
