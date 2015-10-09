583fdcdd996e2a3d05800019691c3867ec26bf0b
maximnelipin <nelipin.maxim@yandex.ru>
2015-10-09 17:42:44 +0300

    Ещё формы.

---
+++ form/additinsthtml.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link rel="stylesheet" type="text/css" href="../stylesheet/reset.css">
+<link rel="stylesheet" type="text/css" href="../stylesheet/general.css">
+<link rel="stylesheet" type="text/css" href="../stylesheet/add.css">
+<title>Добавление ИТ-инструкции</title>
+</head>
+
+    <body>
+    <?php
+	include $_SERVER['DOCUMENT_ROOT'].'/html/header.html';
+	?>
+	    <h2 class="title"> Добавление инструкции для ИТ-спецов</h2>
+	    <form action="?"  method="post">
+	    	<div class="field">
+	    		<label for="name" > Название инструкции</label>
+	    		<input type="text" class="text" size="70" name="name"></textarea>    	
+	    	</div>
+	    	<div class="field">
+	    		<label for="url"> URL-путь к странице с инструкцией</label>
+	    		<input type="text" class="text" size="70"  name="url"></textarea> 
+	    	</div>	    	
+	    	<div>
+	    		<input type="submit" class="button" value="Добавить">
+	    	</div>
+	    	    
+	    </form>
+	    <?php
+	
+		?>
+    </body>
+    
+</html>
---
+++ php_scripts/addusrinst.php
@@ -0,0 +1,46 @@
+<?php
+	session_start();
+	if(isset($_SESSION['user_id']))
+	{	include $_SERVER['DOCUMENT_ROOT'].'/form/addusrinsthtml.php';
+		include 'mysql_conf.php';
+		try {
+			$conbd=new PDO('mysql:host='.$hostsql.';dbname='.$dbname, $dbuser, $dbpwd);
+			$conbd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$conbd->exec('SET NAMES "utf8"');
+		}
+		catch (PDOException $e)
+		{
+			$error= 'Не удалось выполнить запрос'.$e->getMessage();	
+			$urlerr=$_SERVER['PHP_SELF'];
+			//$_SESSION['erroor']=$error;
+			//$_SESSION['urlerr']=$urlerr;
+			include '../form/errorhtml.php';
+			exit;
+		}
+		if (isset($_POST['name']))	
+		{
+			
+			$url=addslashes($_POST["url"]);
+			
+			try {
+				$sql='insert into usrinst set name="'.$_POST["name"].'", url="'.$url.'"';
+				$conbd->exec($sql);
+			}
+			
+			catch (PDOException $e)
+			{
+				
+				$error= 'Не удалось выполнить запрос'.$e->getMessage();	
+				$urlerr=$_SERVER['PHP_SELF'];
+				//$_SESSION['error']=$error;
+				//$_SESSION['urlerr']=$urlerr;
+				include '../form/errorhtml.php';
+				exit;
+			}
+			
+			header('Location .');
+			exit;
+		}	
+	}
+	else header('Location ../index.php');
+?>
