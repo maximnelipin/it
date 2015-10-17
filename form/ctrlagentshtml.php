eba3c6bf877ad898585d902b38350c5e2c241d0d
Maxim Nelipin <nelipin.maxim@yandex.ru>
2015-10-17 18:06:35 +0300

    Стиль для кправляющих страниц

--- form/ctrlagentshtml.php
+++ form/ctrlagentshtml.php
@@ -4,7 +4,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <link rel="stylesheet" type="text/css" href="../stylesheet/reset.css">
 <link rel="stylesheet" type="text/css" href="../stylesheet/general.css">
-<link rel="stylesheet" type="text/css" href="../stylesheet/add.css">
+<link rel="stylesheet" type="text/css" href="../stylesheet/ctrl.css">
 <title>Управление контрагентами</title>
 </head>
 
@@ -13,26 +13,29 @@
 	include $_SERVER['DOCUMENT_ROOT'].'/html/header.html';
 	?>
 	    <h2 class="title"> Управление контрагентами</h2>
-	    <div>
-	    	<a href="?add">Добавить нового контрагента</a>	    
-	    </div>
-	    <ul>
-	    	<?php foreach ($agents as $agent): ?>
-	    	<li>
-	    		<form action="" method="post">
-	    			<div>
-	    			<?php htmlout($agent['name']);?>
-	    			</div>
-	    				<input type="hidden"; name="id" value=<?php echo $agent['id'];?>
-	    			<div>
-		    			<input type="submit" class="button" name="action" value="Редактировать">
-		    			<input type="submit" class="button" name="action" onClick="return confirm('Вы подтверждаете удаление?');" value="Удалить">
-	    			</div>	    		
-	    		</form>
-	    	</li>
-	    	<?php endforeach;?>    
-	    </ul>	    
+	    <div class="field">
 	    
+	    
+		    <div>
+		    	<a href="?add">Добавить нового контрагента</a>	    
+		    </div>
+		    <ul>
+		    	<?php foreach ($agents as $agent): ?>
+		    	<li>
+		    		<form action="" method="post">
+		    			<div class="leftli">
+		    			<?php htmlout($agent['name']);?>
+		    			</div>
+		    				<input type="hidden"; name="id" value=<?php echo $agent['id'];?>>
+		    			<div class="rightli">
+			    			<input type="submit" class="button" name="action" value="Редактировать">
+			    			<input type="submit" class="button" name="action" onClick="return confirm('Вы подтверждаете удаление?');" value="Удалить">
+		    			</div>	    		
+		    		</form>
+		    	</li>
+		    	<?php endforeach;?>    
+		    </ul>	    
+	    </div>
 	    
     </body>
     
