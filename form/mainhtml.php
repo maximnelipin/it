e58093d3c8e0dec72993096515492caaa516e522
maximnelipin <nelipin.maxim@yandex.ru>
2015-10-27 16:53:22 +0300

    Сделал дизайн главной

--- form/mainhtml.php
+++ form/mainhtml.php
@@ -14,59 +14,77 @@
 	include $_SERVER['DOCUMENT_ROOT'].'/php_scripts/func.php';
 	?>
 	    <h2 class="title"> Главная страница</h2>
-	    
-	   <form action=""  method="get" target="_blank" id="sched">
-	    	<div class="field">
-	    		<label for="monyear" > Дежурства</label>
-	    		<select required class="text" size="1" name="monyear">
-	    			<option disabled selected>Выберите месяц</option>
-	    			<?php 
-						
-						$selsql='SELECT Distinct month(dateduty) as month, year(dateduty) as year FROM schedule
-								ORDER BY year, month';
-						$ressql=$condb->query($selsql);
-	    				while ($res=$ressql->fetch(PDO::FETCH_ASSOC))
-	    				{
-	    					$month=numToMonth($res['month']);
-	    					
-	    					echo '<option value='.$res['month'].','.$res['year'].'>'.$month. " ".$res['year'].'</option>';
-	    					    					
-	    				}
-	    				?>
-	    		</select> 	
-	    	
-	    	<div>
-	    	<input type="submit" class="button" size="70" name="schedule" onClick="document.getElementById('sched').action = 'schedule.php'" value="В HTML">
-	    	<input type="submit" class="button" size="70" name="schedpdf" onClick="document.getElementById('sched').action = 'schedpdf.php'" value="В PDF">	    	    	   
-	   		</div>
-	    </div>
-	    </form> 
-	    <form action="usrpc.php"  method="get" target="_blank" >
-	    	<div class="field">
-	    		<label for="usr" > Пользователи</label>
-	    		<select required class="text" size="1" name="usr">
-	    			<option selected value="all">Выберите пользователя</option>
-	    			<?php 
-						
-						$selsql='SELECT login,fio FROM listuser
-								ORDER BY fio';
-						$ressql=$condb->query($selsql);
-	    				while ($res=$ressql->fetch(PDO::FETCH_ASSOC))
-	    				{
-	    					
-	    					
-	    					echo '<option value='.$res['login'].'>'.$res['fio'].'</option>';
-	    					    					
-	    				}
-	    				?>
-	    		</select> 	
-	    	
-	    	<div>
-	    	<input type="submit" class="button" size="70" name="usrpc"  value="Отчёт">    	    	   
-	   		</div>
-	    </div>
-	    </form> 
-	   
+	    <div class="repmain">
+	   		<div class="mainleft">
+			   <form action=""  method="get" target="_blank" id="sched">
+			    	<div >
+			    		<label for="monyear" > Дежурства</label>
+			    		<select required class="text" size="1" name="monyear">
+			    			<option disabled selected>Выберите месяц</option>
+			    			<?php 
+								
+								$selsql='SELECT Distinct month(dateduty) as month, year(dateduty) as year FROM schedule
+										ORDER BY year, month';
+								$ressql=$condb->query($selsql);
+			    				while ($res=$ressql->fetch(PDO::FETCH_ASSOC))
+			    				{
+			    					$month=numToMonth($res['month']);
+			    					
+			    					echo '<option value='.$res['month'].','.$res['year'].'>'.$month. " ".$res['year'].'</option>';
+			    					    					
+			    				}
+			    				?>
+			    		</select> 	
+			    	
+				    	<div>
+					    	<input type="submit" class="button" size="70" name="schedule" onClick="document.getElementById('sched').action = 'schedule.php'" value="В HTML">
+					    	<input type="submit" class="button" size="70" name="schedpdf" onClick="document.getElementById('sched').action = 'schedpdf.php'" value="В PDF">	    	    	   
+					   	</div>
+			    	</div>
+			    </form> 
+		    </div> 
+		    <div class="maincenter">
+			    <form action="usrpc.php"  method="get" target="_blank" >
+			    	<div >
+			    		<label for="usr" > Пользователи</label>
+			    		<select required class="text" size="1" name="usr">
+			    			<option selected value="all">Все Пользователи - Все ПК</option>
+			    			<?php 
+								
+								$selsql='SELECT login,fio FROM listuser
+										ORDER BY fio';
+								$ressql=$condb->query($selsql);
+			    				while ($res=$ressql->fetch(PDO::FETCH_ASSOC))
+			    				{
+			    					
+			    					
+			    					echo '<option value='.$res['login'].'>'.$res['fio'].'</option>';
+			    					    					
+			    				}
+			    				?>
+			    		</select> 	
+			    	
+				    	<div>
+				    		<input type="submit" class="button" size="70" name="usrpc"  value="Отчёт">    	    	   
+				   		</div>
+			    	</div>
+			    </form> 
+		    </div>
+			<div class="mainright">
+				<div>
+				    <a href=gpo.php target="_blank"> GPO </a>
+				</div>
+				<div>
+				    <a href=agent.php target="_blank"> Контрагенты </a>
+				</div>
+				<div>
+				    <a href=itinst.php target="_blank"> Инструкции для ИТ-отдела </a>
+				</div>
+				<div>
+				    <a href=usrinst.php target="_blank"> Инструкции для пользователей </a>
+			    </div>
+			</div>
+		</div>
 	    <div class='ctrl' >
 	    <h3 > Управление</h3>
 	   		 <div class='ctrl_col' >
